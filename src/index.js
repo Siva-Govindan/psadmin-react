805af9a1c66765068ef212ae8fc2a72816ef5162
Raphael Ifiok Akpan <raphael.akpan@andela.com>
2017-07-09 10:14:28 +0100

    add routes file to handle client-side routing

--- src/index.js
+++ src/index.js
@@ -1,2 +1,18 @@
-/* eslint-disable no-console */
-console.log('Hello');
+// Application entry point
+// We will use Router component and pass in routes as props
+
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, browserHistory } from 'react-router';
+
+import routes from './routes';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+render(
+  <Router history={browserHistory} routes={routes} />,
+  document.getElementById('app')
+);
+
+
