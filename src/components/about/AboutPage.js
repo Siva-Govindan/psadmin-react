0dd56720f20d3df235e394515ab6d9a1d3dfd59b
Raphael Ifiok Akpan <raphael.akpan@andela.com>
2017-07-09 10:13:50 +0100

    setup initial components

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <div class="jumbotron">
+        <h1> About </h1>
+        <p> This App anables you to manage courses on Pluralsight. </p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
